334c6ffa6d8c4053701a1f28850d7b652d64df2e
macman1717 <mchawk1717@gmail.com>
2025-02-05 10:47:13 -0500

    fixed stocks/new/<stock_symbol> endpoint

--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -3,7 +3,7 @@ from gc import get_objects
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from . import CsvStockReader as csv
-import json
+
 
 from .models import Stock, EndOfDay
 
@@ -13,9 +13,9 @@ def getStockBySymbol(request,stock_symbol):
     return JsonResponse(csv.getStockData(stock_symbol),safe=False)
 
 def getNewStockBySymbol(request,stock_symbol):
-    data = list(EndOfDay.objects.filter(symbol=stock_symbol.values('date','closing_price')))
+    data = list(EndOfDay.objects.filter(symbol=stock_symbol).values('date','closing_price'))
     return JsonResponse(data,safe=False)
 
 def getStockBySymbolAndRange(request,stock_symbol,start,end):
-    data = list(EndOfDay.objects.filter(symbol=stock_symbol, date__range=[start, end]).values('date', 'closing_price'))
+    data = list(EndOfDay.objects.filter(symbol_id=stock_symbol, date__range=[start, end]).values('date', 'closing_price'))
     return JsonResponse(data, safe=False)
