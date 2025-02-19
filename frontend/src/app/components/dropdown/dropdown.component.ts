44455a61c8be7a1828a02e829575c04f3cab94e1
Sean Nolan <seannolan21307@gmail.com>
2025-02-19 16:07:18 -0500

    added wallet dropdown

---
+++ frontend/src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dropdown',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dropdown.component.html',
+  styleUrl: './dropdown.component.css'
+})
+export class DropdownComponent {
+  @Output() itemClicked = new EventEmitter<string>();
+  @Input() selectedItem!: string;
+  @Input() items!: string[];
+  dropdownOpen = false;
+
+  private wasInside = false;
+
+  @HostListener('click')
+  clickInside() {
+    this.wasInside = true;
+  }
+
+  @HostListener('document:click')
+  clickout() {
+    if (!this.wasInside) {
+      this.dropdownOpen = false;
+    }
+    this.wasInside = false;
+  }
+
+  onClick(item: string) {
+    this.itemClicked.emit(item);
+    this.dropdownOpen = false;
+  }
+}
--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -1,26 +1,35 @@
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ThemeService } from '../../services/theme.service';
+import { DropdownComponent } from "../dropdown/dropdown.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, DropdownComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   showSidebar = true;
   darkMode = false;
+  selectedWallet = 'Wallet 1';
+  walletDropdownOpen = false;
+  wallets = ['Wallet 1', 'Wallet 2'];
 
   constructor(private themeService: ThemeService) {}
-
+  
   ngOnInit() {
     this.themeService.darkMode$.subscribe(isDark => {
       this.darkMode = isDark;
     });
+  }
+
+  changeWallet(wallet: string) {
+    this.selectedWallet = wallet;
+    // window.location.reload();
   }
 
   toggleSidebar() {
