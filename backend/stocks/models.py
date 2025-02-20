2f2b32a019810513c91c55b929beae912974b664
macman1717 <mchawk1717@gmail.com>
2025-02-20 11:18:10 -0500

    refactored stocks app to use api instead of db for EOD data

--- backend/stocks/models.py
+++ backend/stocks/models.py
@@ -6,21 +6,4 @@ from django.db import models
 class Stock(models.Model):
     symbol_id = models.CharField(max_length=10, unique=True, primary_key=True)
     name = models.CharField(max_length=100)
-    description = models.TextField()
 
-# EndOfDay(id, symbol, date, endOfDayPrice, minPrice, maxPrice, volume)
-# 	FK symbol -> Stocks
-class EndOfDay(models.Model):
-    date = models.DateField()
-    closing_price = models.FloatField()
-    open_price = models.FloatField()
-    min_price = models.FloatField()
-    max_price = models.FloatField()
-    volume = models.IntegerField()
-    symbol_id = models.ForeignKey('Stock', on_delete=models.CASCADE)
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["symbol_id", "-date", "closing_price"], name="idx_symbol_date_price")
-        ]
-        unique_together = ("symbol_id", "date")
--- backend/stocks/views.py
+++ backend/stocks/views.py
@@ -1,124 +1,47 @@
-import json
+from datetime import timedelta, date
 
-from django.db.models import Max
-from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView, get_object_or_404
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from .models import Stock
 
-from .models import Stock, EndOfDay
-from .serializers import StockSerializer, EndOfDaySerializer
+import os
+from dotenv import load_dotenv
+import fmpsdk
 
-
-# Create your views here.
-
-class StockView(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.CreateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    queryset = Stock.objects.all()
-    serializer_class = StockSerializer
-    lookup_field = 'symbol_id'
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
+load_dotenv()
+apikey = os.environ.get("API_KEY")
 
 
 @api_view(['GET'])
-def getStockBySymbol(request,stock_symbol):
-    data = dict()
-
-    data['stockInfo'] = StockSerializer(Stock.objects.get(symbol_id=stock_symbol)).data
-    data['prices'] = list(EndOfDay.objects.filter(symbol_id=stock_symbol).values('date','closing_price'))
-    return Response(data)
+def get_stock_details_with_1y_EOD_data(request, symbol):
+    stock_data = fmpsdk.company_profile(apikey=apikey, symbol=symbol)[0]
+    today = date.today()
+    today_minus_year = today - timedelta(days=365)
+    prices = fmpsdk.historical_price_full(apikey=apikey, symbol=symbol, from_date=str(today_minus_year),
+                                          to_date=str(today))
+    close_prices = list()
+    for price in prices:
+        close_prices.append({
+            "date": price["date"],
+            "closing_price": price["close"],
+        })
+    return Response({
+        "stockInfo": stock_data,
+        "prices": close_prices
+    })
 
 @api_view(['GET'])
-def getStockBySymbolAndRange(request,stock_symbol):
+def get_EOD_prices_by_range(request, symbol):
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
-
     if not start_date or not end_date:
         return Response({"error": "start_date and end_date are required."}, status=400)
-
-    stock_prices = EndOfDay.objects.filter(
-        symbol_id=stock_symbol,
-        date__range=[start_date, end_date]
-    ).values('date', 'closing_price')
-
-    return Response(list(stock_prices), status=200)
+    if not symbol:
+        return Response({"error": "symbol is required."}, status=400)
+    return Response(fmpsdk.historical_price_full(apikey=apikey, symbol=symbol, from_date=start_date, to_date=end_date))
 
 @api_view(['GET'])
-def getStocksAndPriceWithChange(request):
-
-    latest_dates = (
-        EndOfDay.objects
-        .values("symbol_id")
-        .annotate(latest_date=Max("date"))
-    )
-
-    stock_data = []
-    for entry in latest_dates:
-        symbol = entry["symbol_id"]
-        current_date = entry["latest_date"]
-
-        current_price = (
-            EndOfDay.objects
-            .filter(symbol_id=symbol, date=current_date)
-            .values_list("closing_price", flat=True)
-            .first()
-        )
-
-        previous_price = (
-            EndOfDay.objects
-            .filter(symbol_id=symbol, date__lt=current_date)
-            .order_by("-date")
-            .values_list("closing_price", flat=True)
-            .first()
-        )
-
-        if current_price is not None and previous_price is not None:
-            change = (current_price / previous_price) - 1
-        else:
-            change = None
-
-        stock_data.append({
-            "symbol": symbol,
-            "current_price": current_price,
-            "change": change
-        })
-
-    return Response(stock_data)
-
-class StockDetailsWithPrices(APIView):
-    def get(self, request, symbol_id):
-        stock = get_object_or_404(Stock, symbol_id=symbol_id)
-        stock_data = StockSerializer(stock).data
-        prices = EndOfDay.objects.filter(symbol_id=symbol_id).values('date', 'closing_price')
-
-        return Response({
-            "stockInfo": stock_data,
-            "prices": list(prices)
-        })
-
-class EndOfDayList(ListAPIView):
-    serializer_class = EndOfDaySerializer
-    def get_queryset(self):
-        symbol_id = self.kwargs['symbol_id']
-        return EndOfDay.objects.filter(symbol_id=symbol_id)
-
-class StockList(generics.ListAPIView):
-    queryset = Stock.objects.all()
-    serializer_class = StockSerializer
+def get_all_supported_symbols(request):
+    stocks = list(Stock.objects.all().values_list('symbol_id', flat=True))
+    return Response(fmpsdk.quote(apikey=apikey, symbol=stocks))
