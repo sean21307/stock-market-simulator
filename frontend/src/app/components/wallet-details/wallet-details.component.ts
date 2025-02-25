f0404f74c486db14e12e1318bb16df94bbdd44c4
Sean Nolan <seannolan21307@gmail.com>
2025-02-25 00:53:57 -0500

    added wip wallet details page

--- frontend/src/app/components/wallet-details/wallet-details.component.ts
+++ frontend/src/app/components/wallet-details/wallet-details.component.ts
@@ -1,21 +1,31 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WalletDetails } from '../../models/walletDetails.model';
+import { WalletService } from '../../services/wallet.service';
+import { CardComponent } from "../card/card.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wallet-details',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './wallet-details.component.html',
   styleUrl: './wallet-details.component.css'
 })
 export class WalletDetailsComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
+  walletDetails!: WalletDetails;
+
+  constructor(private walletService: WalletService) {}
 
   ngOnInit(): void {
     const name = this.route.snapshot.paramMap.get('name');
     if (name) {
-      // will wallets/Sean's Thing work fine for getting details or do we need to use id?
-      console.log(name);
+      this.walletService.getWalletDetails(name).subscribe({
+        next: (wallet: WalletDetails) => {
+          this.walletDetails = wallet;
+        }
+      })
     }
   }
 }
