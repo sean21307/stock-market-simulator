d4c3f027d00c10aec886a5bc06d4b5564b515ee5
Sean Nolan <seannolan21307@gmail.com>
2025-03-14 00:54:38 -0400

    cors fix

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -69,6 +69,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# need this for now because the vercel links have different urls based on the branch
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
 ]
